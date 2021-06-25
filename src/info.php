2c3199d64fa281200c6a4056bc477f996c47a2ef
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2021-06-25 12:40:53 +0500

    feat: сделал возможность получения аргументов

--- src/info.php
+++ src/info.php
@@ -4,27 +4,23 @@ namespace Differ\info;
 
 use Docopt;
 
-
 function getInfo()
 {
     $doc = <<<DOC
-        Generate diff
-
-        Usage:
-            gendiff (-h|--help)
-            gendiff (-v|--version)
-            gendiff <firstFile> <secondFile>
-            
-        Options:
-            -h --help                     Show this screen
-            -v --version                  Show version
-            --format <fmt>                Report format [default: stylish]
-    DOC;
-
-    $args = Docopt::handle($doc, array(
-        'version' => null,
-        'help' => true,
-    ));
+    Generate diff
     
+    Usage:
+        gendiff (-h|--help)
+        gendiff (-v|--version)
+        gendiff [--format <fmt>] <firstFile> <secondFile>
+        
+    Options:
+        -h --help                     Show this screen
+        -v --version                  Show version
+        --format <fmt>                Report format [default: stylish]
+    DOC;
+    
+    $args = Docopt::handle($doc, array('version'=>'Naval Fate 2.0'));
+
     return $args;
 }
