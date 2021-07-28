b5864877112a5bb2d53f275965ceb9dfb8dc5798
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2021-07-28 14:02:18 +0500

    test

--- src/Parser.php
+++ src/Parser.php
@@ -8,6 +8,8 @@ function getDataFromFile(string $filepath)
 {
     $fileExtension = substr($filepath, -4);
 
+    print_r($filepath);
+
     switch ($fileExtension) {
         case 'json':
             return json_decode(file_get_contents($filepath), true);
